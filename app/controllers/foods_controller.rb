f9f3d410e6c22019b5fc5d4edbe1357c7c941206
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-31 16:33:53 +0300

    Add Delete button

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,6 +8,10 @@ class FoodsController < ApplicationController
     @food = Food.new
   end
 
+  def show
+    @food = Food.find_by_id(params[:id])
+  end
+
   def create
     @food = Food.create(foods_params)
     @food.user_id = current_user.id
@@ -16,6 +20,16 @@ class FoodsController < ApplicationController
       redirect_to foods_path
     else
       render :new, status: 422
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+
+    if @food.destroy
+      redirect_to foods_path, notice: 'Food was successfully deleted.'
+    else
+      redirect_to foods_path, alert: 'There was an error deleting the Food.'
     end
   end
 
