181882020c2f06ccbfdd720838a7b6c1f9a3e575
citec-47 <mauricendonyi40@gmail.com>
2023-08-01 00:59:58 -0700

    Recipies.html.erb

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -1,4 +1,8 @@
 class RecipiesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :public_recipies]
+  def index
+    @recipies = Recipe.where(user_id: current_user.id)
+  end
   def public_recipies
     @public = Recipe.where(public: true).order('created_at DESC')
   end
