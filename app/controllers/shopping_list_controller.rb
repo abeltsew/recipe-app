52099faf17607951edb3c54f57a7e9a0d0ebe3f8
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-01 14:47:52 +0300

    Add shopping list

---
+++ app/controllers/shopping_list_controller.rb
@@ -0,0 +1,10 @@
+class ShoppingListController < ApplicationController
+  def index
+    @recipies = Recipe.where(user_id: current_user.id)
+    @foods1 = []
+    @recipies.each do |recipie|
+        @foods1 << Food.joins(:recipe_foods).where(recipe_foods: { recipe_id: recipie.id })
+    end
+    @foods = @foods1[0]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
   get '/public_recipies', to: 'recipies#public_recipies'
+  get '/shopping_list', to: 'shopping_list#index'
   root 'recipies#public_recipies'
 end
