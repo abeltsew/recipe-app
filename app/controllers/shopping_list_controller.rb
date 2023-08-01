c55d08d890d1fc5786c7a35f47673fda095d00c3
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-01 17:24:00 +0300

    Update shopping list

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,10 +1,6 @@
 class ShoppingListController < ApplicationController
   def index
-    @recipies = Recipe.where(user_id: current_user.id)
-    @foods1 = []
-    @recipies.each do |recipie|
-      @foods1 << Food.joins(:recipe_foods).where(recipe_foods: { recipe_id: recipie.id })
-    end
-    @foods = @foods1[0]
+    @recipes = current_user.recipies.includes(recipe_foods: :food)
+    @recipe_foods = @recipes.map(&:recipe_foods).flatten
   end
 end
