377da1d51f7d77206cff2c4d9d4b924a320ebd49
citec-47 <mauricendonyi40@gmail.com>
2023-08-01 01:35:50 -0700

    Add delete reciepies button

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -6,4 +6,14 @@ class RecipiesController < ApplicationController
   def public_recipies
     @public = Recipe.where(public: true).order('created_at DESC')
   end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.destroy
+      redirect_to recipies_path, notice: 'Recipe was successfully deleted.'
+    else
+      redirect_to recipies_path, alert: 'There was an error deleting the Recipe.'
+    end
+  end
 end
