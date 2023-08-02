d83155352fa303f96a707664ca4ab1e65df9560f
citec-47 <mauricendonyi40@gmail.com>
2023-08-02 01:31:49 -0700

    Generating Ingredient

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -6,7 +6,7 @@ class RecipiesController < ApplicationController
 
   def show
     @recipe = Recipe.find_by(id: params[:id])
-    @foods = Food.joins(:recipe_foods).where(recipe_foods: { recipe_id: @recipe.id })
+    @foods = Food.joins(:recipe_foods).where(recipe_foods: { recipe_id: @recipe.id }).includes([:recipe_foods])
   end
 
   def new
