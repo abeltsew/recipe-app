20ed792141cd1446c303d27bfee487855e257fac
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-02 10:56:21 +0300

    update specs

--- spec/requests/food_spec.rb
+++ spec/requests/food_spec.rb
@@ -4,9 +4,9 @@ require 'faker'
 describe Food, type: :request do
   describe ' foods route should be available' do
     it 'should render the index action correctly' do
-      get '/foods'
+      get '/'
       expect(response).to be_successful
-      expect(response).to render_template('foods/index')
+      expect(response).to render_template('recipies/public_recipies')
     end
   end
 end
